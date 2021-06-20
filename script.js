788dc83657b297ff64f8a7f61bdb74df6c2c9225
moshtaghfr <moshtagh.fr@gmail.com>
2021-06-20 19:04:09 +0200

    finish to do list

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
 //simple shop ing list
-const button = document.getElementById('enter');
-let input = document.getElementById('userinput');
-let ul = document.querySelector('ul');
-const inputLenght = () => {
-  return input.value.length;
-};
-const createListElement = () => {
-  let li = document.createElement('li');
-  li.appendChild(document.createTextNode(input.value));
-  ul.appendChild(li);
-  input.value = '';
-};
-const addListAfterClick = () => {
-  if (inputLenght() > 0) {
-    createListElement();
-  }
-};
-const AddListAfterKeypress = (event) => {
-  if (input.value.length > 0 && event.key === 'Enter') {
-    createListElement();
-  }
-};
-button.addEventListener('click', addListAfterClick);
+var button = document.getElementById('enter');
+var input = document.getElementById('userinput');
+var ul = document.querySelector('ul');
 
-input.addEventListener('keypress', AddListAfterKeypress);
+function createListElement() {
+  var div = document.createElement('div');
+  var li = document.createElement('li');
+  var delButton = document.createElement('button');
+  div.classList.add('wrapper');
+  ul.appendChild(div);
+  div.append(li, delButton);
+  li.classList.add('taskClass');
+  li.appendChild(document.createTextNode(input.value));
+  input.value = '';
+  delButton.classList.add('delClass');
+  delButton.innerHTML = 'Del';
+}
+
+function inputLength() {
+  return input.value.length;
+}
+
+function addListAfterClick() {
+  if (inputLength() > 0) {
+    createListElement();
+  }
+}
+
+function addListAfterKeypress(event) {
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
+}
+
+function doneTask(task) {
+  if (task.target.tagName === 'LI') {
+    task.target.classList.toggle('done');
+  }
+}
+
+function deleteListElement(element) {
+  if (element.target.className === 'delClass') {
+    element.target.parentElement.remove();
+  }
+}
+
+function handleUlClick(element) {
+  doneTask(element);
+  deleteListElement(element);
+}
+
+ul.addEventListener('click', handleUlClick);
+button.addEventListener('click', addListAfterClick);
+input.addEventListener('keypress', addListAfterKeypress);
+
+//
